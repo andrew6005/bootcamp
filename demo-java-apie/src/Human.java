16038ae1606e06fe23e525908ee0bc99ed91679a
andrew6005 <andrew06052015@gmail.com>
2026-04-13 17:35:48 +0800

    2026-04-13

---
+++ demo-java-apie/src/Human.java
@@ -0,0 +1,14 @@
+public abstract class Human {
+  private String name;
+
+  public void String(){
+    
+  }
+   public String setName(String string){
+    return this.name;
+    
+   }
+   public  void  getName(){
+     this.name=name;
+   }
+}
--- demo-java-apie/src/Superman.java
+++ demo-java-apie/src/Superman.java
@@ -1,5 +1,5 @@
 //interface
-public class Superman implements flyable {
+public class Superman extends Human implements flyable , Swimable{
   private int hp;
   private int attackValue;
   @Override
@@ -8,6 +8,10 @@ public class Superman implements flyable {
   }
   public void sleep(){
     System.out.println("superman is Sleeping");
+  }
+  @Override
+  public void swim(){
+    System.out.println("superman is swiming");
   }
 
   public Superman(){
@@ -30,7 +34,7 @@ public class Superman implements flyable {
   }
   public void deducthp(int tobededucthp){
     if(this.hp >= tobededucthp){
-      this.hp - tobededucthp;
+      this.hp =tobededucthp;
     }else{
       this.hp = 0;
     }
@@ -40,8 +44,9 @@ public class Superman implements flyable {
   public int getHp(){
     return this.hp;
   }
+ 
   public static void main(String[] args) {
-    Superman s1 = new Superman();
+    /*Superman s1 = new Superman();
     //s1.sleep();
     //s1.fly();
     s1.deducthp(100);
@@ -53,7 +58,12 @@ public class Superman implements flyable {
     s1.attack(s2);
     s2.deducthp(100);
     System.out.println(s2.isAlive());
-    System.out.println(s2.getHp());
+    System.out.println(s2.getHp());*/
+    Superman s3 =new Superman();
+
+    s3.setName("john");
+    //super();
+  System.out.println(s3.getClass().getName());
 
 
     
